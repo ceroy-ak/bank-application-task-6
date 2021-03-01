552aa12bf695ede19ee61e4de11f4b44afea5820
Ceroy <abhishek.k@technovert.com>
2021-03-01 10:24:45 +0530

    Regex changes

---
+++ src/client/common/services/client.validation.regex.ts
@@ -0,0 +1,3 @@
+export const nameRegex = /^[a-zA-Z][a-zA-Z ]+$/
+export const usernameRegex = /^[a-z0-9]{6,}$/
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
